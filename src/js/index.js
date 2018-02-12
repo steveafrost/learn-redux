3c0add8fbb5ecca9c8f497970b859d8091df84fa
Steve Frost <sfrost@nerdery.com>
2018-02-12 12:18:36 -0600

    expose store & addArticle action to window for console fun

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import store from '../js/store/index';
+import { addArticle } from '../js/actions/index';
 
 const App = () => <h1>Setting up React with Parcel bundler</h1>;
+
 const wrapper = document.getElementById('app');
+
 wrapper ? render(<App />, wrapper) : false;
+
+window.store = store;
+window.addArticle = addArticle;
